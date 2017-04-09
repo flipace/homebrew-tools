4f1a9bfb876f798cdafda387ae88a71fb13b8e8e
Patrick Neschkudla <pn@ovos.at>
2017-04-09 21:29:59 +0200

    updated hantera to 0.0.4

--- hantera.rb
+++ hantera.rb
@@ -1,8 +1,8 @@
 class Hantera < Formula
   desc "easily manage projects which utilize a service oriented architecture"
   homepage "https://hantera.neschkudla.at"
-  url "https://github.com/flipace/hantera/archive/v0.0.3.tar.gz"
-  sha256 "b640da6c9d1c4c2cdac157a89a714cb5798104bd39825f8734b6ab146b95b73a"
+  url "https://github.com/flipace/hantera/archive/v0.0.4.tar.gz"
+  sha256 "02047bdd2dbf2e6cf836657b626edd19b3166d2211b28168360beebdde79f441"
 
   bottle :unneeded
 
