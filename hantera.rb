3ca10aa480b82ee54653a7992d78ea24a279cb60
Patrick Neschkudla <pn@ovos.at>
2017-04-10 11:13:18 +0200

    fix shasum

--- hantera.rb
+++ hantera.rb
@@ -2,7 +2,7 @@ class Hantera < Formula
   desc "easily manage projects which utilize a service oriented architecture"
   homepage "https://hantera.neschkudla.at"
   url "https://github.com/flipace/hantera/archive/v0.0.5.tar.gz"
-  sha256 "8086c957aa34e1cd3c093f26a9576a4c6d882836ebb125a1aa11b34b860e51c8"
+  sha256 "81b313d3ab93673759715efe9c3cf209437ecde85554ee6f0ccd4e3fa617d68d"
 
   bottle :unneeded
 
